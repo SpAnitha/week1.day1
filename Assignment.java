307b2cd5c8e5842d29f453ffa0bdfe57525cf900
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-04-22 19:43:17 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+package week.day1;
+
+public class Assignment {
+public static void main(String[] args) {
+	int a=-40;
+	int b=0;
+	System.out.println(a<=b);
+	System.out.println("The number -40 is converted to 40");
+}
+}
+
---
+++ Assignment5.java
@@ -0,0 +1,22 @@
+package week.day1;
+
+public class Assignment5{
+	public static void main(String[] args) {
+	int input=13;
+	boolean flag=false;
+	for(int i =2;i<=input/2;i++) {
+      if( i==0) {
+		   flag = true;
+			break;
+		}
+	}
+if(!flag) {
+	System.out.println("prime");
+}
+else {
+	System.out.println("not prime");
+}
+}	
+}
+
+
