e2a4db819e4cad3b84931887c4144d7f89858f53
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-04-22 19:47:27 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,12 @@
+package week.day1;
+
+public class Assignment4 {
+public static void main(String[]args) {
+	int number=6, fact=1;
+	for(int i=1;i<=number;i++)
+	{
+		fact=fact*i;
+}
+	System.out.println("factorial of the given"+ number+ "is"+ fact);
+}
+}
