4aa88a716e3c311053ec116e2cbc084eee6cca70
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-04-22 20:15:21 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,20 @@
+package week.day1;
+
+public class Assignment2{
+	public static void main(String[] args) {
+		int num=35;
+		if(num>0) {
+		System.out.println("Number is positive");
+		}
+		else{
+			if(num==0) {
+		System.out.println("Number is neither positive nor negative");
+		}
+		else
+		 {
+		System.out.println("Number is negative");
+	}
+	}
+}
+}
+
