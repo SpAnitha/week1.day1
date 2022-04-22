509f53581ad0bce8a0874ef6ce3731fbd4a178d9
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-04-22 21:05:42 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+package week.day1;
+
+public class Assignment {
+public static void main(String[] args) {
+	int a=-40;
+	if(a>=0) {
+		System.out.println("The number is negative");
+	}
+	else
+	{
+	
+	System.out.println("The number -40 is converted to 40 ");
+
+}	
+}
+}
+
