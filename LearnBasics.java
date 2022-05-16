f1063b8e0a08d1f16333f426ca48ce1ecd19bf5a
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-05-16 22:13:14 +0530

    Add files via upload

---
+++ LearnBasics.java
@@ -0,0 +1,12 @@
+package week.day1;
+
+public class LearnBasics {
+	byte pixelSize=100;
+	short internalStorage=3200;
+	int imei=987654874;
+	long mobileNumber=276859L;
+	float secondaryPixelSize=63498F;
+	double number=78654;
+	char modelnumber='1';
+	boolean isMobilecharged=true;
+}
